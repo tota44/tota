02ce0f1abff504dab64940df26cde157439d579a
tota44 <74247512+tota44@users.noreply.github.com>
2020-11-10 16:33:41 +0200

    Add files via upload

---
+++ WH2.cpp
@@ -0,0 +1,17 @@
+\\initilizing multidimensional _arrays 
+_#include <iostream>
+#include(string)
+int main()
+\\continue
+{
+int array 1[1] [10]={1,2,3,4,5,6,7,8,9,10}
+str array 2 [1 [8]]={a,b,c,d,e,f,g,h}
+int sum ,average
+cout<<"sum is: "<<array 1;
+cin>>array 1;
+cout<<"sum is: "<<array 2;
+cin>>array 2;
+cout<<"average is: " ,array1,<<sum\10;
+cout<<"average is: ",array 2,<<sum\8;
+return 0;
+}
