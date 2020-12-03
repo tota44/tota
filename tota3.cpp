e09bbcdc5a60c3c9eca250de1874f7a0749c0c15
tota44 <74247512+tota44@users.noreply.github.com>
2020-12-03 17:01:32 +0200

    Add files via upload

---
+++ tota3.cpp
@@ -0,0 +1,41 @@
+\* My name: Tayseer yahia Abdalla Mohammed 
+Class: The Second
+Department: Computer Engineering*/
+#include <bit \stdc++.h>
+using namespace std;
+struct Queue{
+    stack <int> s1,s2;
+    void enQueue(int x)
+}
+while(!s1.empty()){
+    s1.push(s1.top())
+    s2.pop();
+}
+s1.push (x);
+while(!s2.empty()){
+    s1.push(s2.top());
+    s2.pop();
+}
+}
+int deQueue(){
+    if(s1.Empty()){
+        cout<<"Q is Empty";
+        exit(0);
+    }
+    int x =s1.top();
+    s1.pop();
+    return x;
+}
+};
+int main (){
+    Queue q;
+    q.enQueue(1);
+    q.enQueue(2);
+    q.enQueue(3);
+    cout<<q.deQueue()<<'\n';
+    cout<<q.deQueue()<<'\n';
+    cout<<q.deQueue()<<'\n';
+    return 0;
+
+
+}
